b516722acd50c005557414e1813559e47a797f36
DESKTOP-UQ2F4N6\jayde <jaydee34551@gmail.com>
2025-11-16 21:26:59 +0800

    fix

---
+++ BusTicketingAndReservationSystem/src/busticketingandreservationsystem/BusTicketingAndReservationSystem.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package busticketingandreservationsystem;
+
+import view.LoginFrame;
+
+/**
+ *
+ * @author jayde
+ */
+public class BusTicketingAndReservationSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+    }
+    
+}
